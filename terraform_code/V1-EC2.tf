525cbeb34faa16a0bb108322cd9ee38e728c81d1
vikramDevPrac <rautvikram007@gmail.com>
2024-11-21 21:45:49 +0530

    commiting initial v1-ec2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+  ami = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  key_name = "devops-project2"
+  
 }
